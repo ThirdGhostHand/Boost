525a81702ba53285be9034c07d89eb603aeb24f1
ThirdGhostHand <ThirdGhostHand@outlook.com>
2018-01-13 01:11:28 -0800

    Added base header comp
    Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import Landing from "./landing";
+import Header from "./header";
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
         <Landing />
+        <Header />
       </div>
     );
   }
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./header.css";
+
+const Header = () => {
+  return <div className="Header">Header</div>;
+};
+
+export default Header;
